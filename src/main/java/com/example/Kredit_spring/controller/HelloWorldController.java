10ce9d119760be72d4c02c0354d85347739f962a
Julia_pc <fialka_jul@mail.ru>
2021-06-24 15:01:09 +0400

    добавлена таблица кредитов

--- src/main/java/com/example/Kredit_spring/controller/HelloWorldController.java
+++ src/main/java/com/example/Kredit_spring/controller/HelloWorldController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Slf4j
 @Controller
 public class HelloWorldController {
-    @RequestMapping("/greeting")
+    @RequestMapping("/klientForm")
     public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         log.info("com.example.Kredit_spring.controller.start");
         model.addAttribute("name", name);
-        return "greeting";
+        return "klientForm";
     }
 }
